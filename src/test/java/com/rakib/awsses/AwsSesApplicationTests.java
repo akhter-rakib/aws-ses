ebbc7f1749832ce0b9f986f299680fa79419b713
aktaruzzaman.rakib <aktaruzzaman.rakib@bjitgroup.com>
2022-03-07 17:40:44 +0600

    git commit -m "initiate with aws-ses dependency"

---
+++ src/test/java/com/rakib/awsses/AwsSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.awsses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
